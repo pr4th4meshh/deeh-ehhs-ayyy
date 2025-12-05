62b782d53d5585c15951a3bd88c0cf4031687c13
Prathamesh Asolkar <87704191+pr4th4meshh@users.noreply.github.com>
2025-12-05 21:50:42 +0530

    Time: 8 ms (74.01%) | Memory: 12.9 MB (36.09%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -5,19 +5,18 @@ class Solution(object):
         :rtype: List[int]
         """
         flat = [x for row in grid for x in row]
-        print("flat", flat)
         repeating, missing = -1, -1
         n = len(flat)
+        grid_bool = [0 for _ in range(n)]
+        for element in flat:
+                if element == grid_bool[element-1]:
+                    repeating = element
+                else:
+                    grid_bool[element-1] = element
 
-        for i in range(1, n+1):
-            print("i", i)
-            count = flat.count(i)
-            print("count", count)
-
-            if count == 2:
-                repeating = i
-            elif count == 0:
-                missing = i
-
+        for index in range(n):
+            if grid_bool[index] == 0:
+                missing = index + 1
+                break
+        
         return [repeating, missing]
-            
