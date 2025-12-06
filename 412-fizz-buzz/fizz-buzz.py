ff3a80dc491644763fd50ac947a8814628678136
Prathamesh Asolkar <87704191+pr4th4meshh@users.noreply.github.com>
2025-12-06 21:51:01 +0530

    Time: 1 ms (61.95%) | Memory: 13.2 MB (86.20%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -5,18 +5,10 @@ class Solution(object):
         :rtype: List[str]
         """
     
-        result = []
-
-        for i in range(1, n + 1):
-            if i % 3 == 0 and i % 5 == 0:
-                result.append("FizzBuzz")
-            elif i % 3 == 0:
-                result.append("Fizz")
-            elif i % 5 == 0:
-                result.append("Buzz")
-            else:
-                result.append(str(i))
-
-        return result
-            
-        
+        return [
+            "FizzBuzz" if i % 15 == 0
+            else "Fizz" if i % 3 == 0
+            else "Buzz" if i % 5 == 0
+            else str(i)
+            for i in range(1, n + 1)
+        ]
