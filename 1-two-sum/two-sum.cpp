c170d222a9295bb693eb1c5c63412a54eeecd5c5
Prathamesh Asolkar <87704191+pr4th4meshh@users.noreply.github.com>
2025-12-12 17:55:20 +0530

    Time: 22 ms (37.84%) | Memory: 14.9 MB (37.14%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,13 +1,17 @@
+using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int len = nums.size();
-        for(int i = 0; i < len; i++){
-            for(int j = i+1; j < len; j++) {
-                if(nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+        unordered_map<int, int> hashMap;
+        for(int i=0; i < nums.size(); i++) {
+            int ans = target - nums[i];
+            cout<<ans<<endl;
+            if(hashMap.count(ans)) { // returns 1 if true or 0
+                return {
+                    hashMap[ans], i // hashMap[invalid_index], index_where_el_was_found
+                };
             }
+            hashMap[nums[i]] = i; 
         }
         return {};
     }
