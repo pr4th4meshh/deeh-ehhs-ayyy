66bbdc449fab4fe47e48980d6d3664ca36602ba1
Prathamesh Asolkar <87704191+pr4th4meshh@users.noreply.github.com>
2025-12-17 22:18:10 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (59.65%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -5,17 +5,37 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        arr = [[0] * n for _ in range(m)]
+        dp = [[0] * n for _ in range(m)]
         for i in range(m):
             for j in range(n):
-                print(arr)
-                if i == 0 or j == 0:
-                    arr[i][j] = 1
-                else:
-                    arr[i][j] = arr[i][j-1] + arr[i-1][j]
+                # Base condition
+                if i == 0 and j == 0:
+                    dp[i][j] = 1
+                    """Skip the rest of the loop and 
+                    continue with the next iteration."""
+                    continue
 
-        
-        return arr[i][j]
+                """ Initialize variables to store the number
+                of ways from cell above (up) and left (left)"""
+                up = 0
+                left = 0
+
+                """ If we are not at first row (i > 0), update 
+                'up' with the value from the cell above"""
+                if i > 0:
+                    up = dp[i - 1][j]
+
+                """ If we are not at the first column (j > 0),
+                update 'left' with value from the cell to left"""
+                if j > 0:
+                    left = dp[i][j - 1]
+
+                """ Calculate the number of ways to reach the
+                current cell by adding 'up' and 'left'"""
+                dp[i][j] = up + left
+
+        # The result is stored in bottom-right cell (m-1, n-1)
+        return dp[m - 1][n - 1]
 
 
 
