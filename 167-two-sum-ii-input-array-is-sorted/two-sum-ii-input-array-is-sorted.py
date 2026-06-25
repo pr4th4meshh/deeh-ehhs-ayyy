8a8ce68370b9bdd18498e275e527635e66d08fe5
Prathamesh Asolkar <87704191+pr4th4meshh@users.noreply.github.com>
2026-06-25 19:27:58 +0530

    Time: 4 ms (48.96%) | Memory: 20.3 MB (94.78%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,14 +1,22 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        left = 0
+        # LEFT POINTER
+        left = 0 
+        # RIGHT POINTER
         right = len(numbers) - 1
 
+        # WHILE LEFT POINTER IS LESS THAT RIGHT POINTER
         while left < right:
+            # INIT CURRENT SUM AS TOTAL OF FIRST AND LAST ELEMENT IN THE ARRAY
             currSum = numbers[left] + numbers[right]
             if currSum > target:
+            # MOVE RIGHT POINTER TO LEFT
                 right -= 1
             elif currSum < target:
+            # MOVE LEFT POINTER TO RIGHT
                 left += 1
             else:
+            # ADD + 1 TO INDEX BECAUSE THE numbers ARRAY'S INDEX IS FROM 1
                 return[left + 1, right + 1]
+        # BASE RETURN EMPTY ARRAY
         return []
