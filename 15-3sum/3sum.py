475bc5a7c4ce6551fe5da613ba9df9ace43be069
Prathamesh Asolkar <87704191+pr4th4meshh@users.noreply.github.com>
2026-07-11 15:18:58 +0530

    Time: 577 ms (77.17%) | Memory: 22.4 MB (31.88%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,33 +1,23 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         res = []
-        nums.sort()  # Sort to enable two-pointer approach
+        # sort the nums arr
+        nums.sort()
 
         for i, a in enumerate(nums):
-            # Skip duplicate first elements to avoid duplicate triplets
             if i > 0 and a == nums[i - 1]:
                 continue
-
-            left, right = i + 1, len(nums) - 1
-
+            
+            left , right = i + 1, len(nums) - 1
             while left < right:
-                total = a + nums[left] + nums[right]
-
-                if total > 0:
-                    # Sum is too large, decrease it
+                threeSum = a + nums[left] + nums[right]
+                if threeSum > 0:
                     right -= 1
-
-                elif total < 0:
-                    # Sum is too small, increase it
+                elif threeSum < 0:
                     left += 1
-
                 else:
-                    # Found a valid triplet
                     res.append([a, nums[left], nums[right]])
                     left += 1
-
-                    # Skip duplicate second elements
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1    
         return res
