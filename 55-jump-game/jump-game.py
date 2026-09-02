d08f7dd5093f3a8320a047d3b28cfd37f7037640
Prathamesh Asolkar <87704191+pr4th4meshh@users.noreply.github.com>
2026-09-02 17:42:53 +0530

    Time: 14 ms (80.46%) | Memory: 20.3 MB (62.52%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,6 +1,7 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        goal = len(nums) - 1
+        # solve w backward greedy
+        goal = len(nums) - 1 # last index of nums
 
         for i in range(len(nums) - 2, -1 , -1):
             if i + nums[i] >= goal:
